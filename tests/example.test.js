7fae91af8fbcd1636cd50e10d492ad038fea7ff0
Marvin krieg <marvin.krieg@lehre.roche.com>
2025-11-21 12:45:30 +0100

    format fix

--- tests/example.test.js
+++ tests/example.test.js
@@ -3,5 +3,5 @@ const test = require('node:test');
 const assert = require('node:assert');
 
 test('basic math works', () => {
-    assert.strictEqual(2 + 2, 4);
+  assert.strictEqual(2 + 2, 4);
 });
